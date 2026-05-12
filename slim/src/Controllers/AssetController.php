ac9eeda3795a0df2be1066a99c451d5b8fdbca71
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-05-12 15:36:09 -0300

    Activos - Controller: GET - Activos, ActivoHistorico

--- slim/src/Controllers/AssetController.php
+++ slim/src/Controllers/AssetController.php
@@ -11,15 +11,13 @@ class AssetController {
     // GET ACTIVOS
     public function getAssets(Request $request, Response $response) {
 
-        //$asset = $request->getAttribute('asset');
-
         $params = $request->getQueryParams();
 
         $name = $params['name'] ?? null;
         $min_price = $params['min_price'] ?? null;
         $max_price = $params['max_price'] ?? null;
 
-        $sql = "SELECT * FROM assets WHERE 1=1";
+        $sql = "SELECT id, name, current_price FROM assets WHERE 1=1";
         $values = [];
 
         if(!empty($name)) {
@@ -49,7 +47,38 @@ class AssetController {
         $assets = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $response->getBody()->write(json_encode($assets));
-        return $response;
+        return $response->withStatus(200);
 
+    }
+
+    // GET HISTORIAL ACTIVO
+    public function getAssetHistory(Request $request, Response $response, $args) {
+        
+        $assetId = $args['asset_id'];
+        $quantity = (int)$args['quantity'];
+
+        if(!is_numeric($assetId)) {
+            $response->getBody()->write(json_encode(["error" => "ID de activo inválido."]));
+            return $response->withStatus(400);
+        }
+
+        if($quantity <= 0) $quantity = 1;
+
+        $quantity = min($quantity, 5);
+
+        $pdo = Database::PDO();
+
+        $stmt = $pdo->prepare("SELECT transaction_type, quantity, price_per_unit, total_amount, transaction_date FROM transactions WHERE asset_id = ? ORDER BY transaction_date DESC LIMIT $quantity");
+        $stmt->execute([$assetId]);
+
+        $history = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if(empty($history)) {
+            $response->getBody()->wirte(json_encode(["error" => "No existen movimientos de ese activo."]));
+            return $response->withStatus(404);
+        }
+
+        $response->getBody()->write(json_encode($history));
+        return $response->withStatus(200);
     }
 }

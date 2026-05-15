f4c2d27cf5fc0bf0f6f55c20a928a24ea49d4247
JuanCruzRusconi <juanrusconi@hotmail.es>
2026-05-15 10:18:51 -0300

    Activos - Routes: GET & PUT Activos

--- slim/src/Routes/AssetRoutes.php
+++ slim/src/Routes/AssetRoutes.php
@@ -1,10 +1,16 @@
 <?php
 
 use Slim\App;
+use App\Middlewares\AuthMiddleware;
 use App\Controllers\AssetController;
 
 return function(App $app) {
 
     $app->get('/assets', [AssetController::class, 'getAssets']);
+
+    $app->put('/assets', [AssetController::class, 'putAssets'])
+        ->add(new AuthMiddleware());
+
+    $app->get('/assets/{asset_id}/history/{quantity}', [AssetController::class, 'getAssetHistory']);
 
 };
